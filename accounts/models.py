8dbc854fb6189349e08fea6cc69d19dc0af55dae
Jakhongir <jahongirbaxtiyorovich17@gmail.com>
2024-03-07 09:06:04 +0500

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The username must be set')
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        # Ensure that only superusers can be created using this method
+        extra_fields.setdefault('type', 'S')
+
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    USER_TYPES = (
+        ('S', 'Superuser'),
+        ('E', 'Employer'),
+        ('C', 'Candidate'),
+    )
+    objects = UserManager()
+
+    REQUIRED_FIELDS = []
+    type = models.CharField(choices=USER_TYPES, default='S', max_length=1)
---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from accounts.models import CustomUser
+
+
+def post_login(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'),
+                            password=request.POST.get('password'))
+        if user:
+            if user.type == 'E':
+                pass
+            elif user.type == 'C':
+                pass
+        else:
+            print('User is not authenticated')
+    return render(request, 'login.html')
+
+
+def registration(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        user = CustomUser(
+            username=request.POST.get('username'),
+            first_name=request.POST.get('first'),
+            last_name=request.POST.get('last'),
+            type=request.POST.get('role'),
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('/login')
+
+    return render(request, 'registration.html')
